a07deaed7a83a9b10ed93603460b202d64d23047
Dewanshi-Patel <Dewpatel2@gmail.com>
2022-05-16 12:17:11 -0700

    git today

---
+++ GitTest.java
@@ -0,0 +1,13 @@
+package variousConcepts;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("git test 1");
+		System.out.println("dewanshi Project");
+		System.out.println("dewanshi J Project");
+		System.out.println("dewanshi P Makadia");
+		
+	}
+	}
+	
